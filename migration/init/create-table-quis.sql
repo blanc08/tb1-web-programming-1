8c86a279503ae7788a912481cee238a77a999279
mfaizalsyahroni <121980257+mfaizalsyahroni@users.noreply.github.com>
2023-04-09 11:28:36 +0700

    PLUS INT

--- migration/init/create-table-quis.sql
+++ migration/init/create-table-quis.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS quizzes (
-    id NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     nama VARCHAR(50) NOT NULL,
     mata_pelajaran VARCHAR(50) NOT NULL,
     timer INT(30) NOT NULL 
